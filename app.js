d88a4cb1aeb7f79cd86a5f1526815471c8f73778
Vera Fesianava <verafes@gmail.com>
2025-06-19 19:25:07 -0700

    start: Node/Express Final Project

---
+++ app.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+require("express-async-errors");
+
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const express = require("express");
+
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
+
+const storiesRouter = require("./routes/stories");
+const authRouter = require("./routes/auth");
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+const app = express();
+app.use(express.json());
+app.set("trust proxy", 1);
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+// routes
+app.use(express.static("public"));
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/stories", authenticateUser, storiesRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
